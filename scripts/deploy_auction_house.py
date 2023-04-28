588cdf707ca0a9d708f8b5515d899278f9a50a73
Rask467 <95650694+Rask467@users.noreply.github.com>
2023-04-28 08:04:32 -0600

    Change constructor arguments auction house deploy

--- scripts/deploy_auction_house.py
+++ scripts/deploy_auction_house.py
@@ -3,11 +3,11 @@ from brownie import LlamaAuctionHouse, accounts, web3
 
 def main():
     acct = accounts.load("llama_deployer")
-    llamas_addr = "0xe433755F2740684233A0283356b522f9249515DD"
+    llamas_addr = "0xe127cE638293FA123Be79C25782a5652581Db234"
     time_buffer = 300
-    reserve_price = web3.toWei(0.1, "ether")
+    reserve_price = web3.toWei(0.2, "ether")
     min_bid_increment_percentage = 2
-    duration = 25200  # 7 hours in seconds
+    duration = 5400
     LlamaAuctionHouse.deploy(
         llamas_addr,
         time_buffer,
